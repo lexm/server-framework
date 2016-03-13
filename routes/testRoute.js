df868f53b6e3d651ea0480bf0b6057471691c75e
Lex Myers <lex.myers@gmail.com>
2016-03-13 13:36:47 -0700

    extended routing

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -1,13 +1,43 @@
 'use strict';
 
-// var fs = require('fs');
+var fs = require('fs');
 // var http = require('http');
 // var router = require('../lib/router');
 // var mainRouter = router.mainRouter;
 var mainRouter = require('../server').mainRouter;
 
+var nextId = 0;
+
+var writeLog = function(id, chunk) {
+  var padId = ('000000' + id).substr(-6);
+  console.log(padId);
+  var newFilename = __dirname + '/../data/' + padId + 'test.json';
+  fs.writeFile(newFilename, chunk.toString(), () => {
+    console.log(newFilename + ' finished writing');
+  });
+};
+
 mainRouter.post('/test/', (req, res) => {
   console.log('test POST hit');
+  req.on('data', (data) => {
+    if(nextId == 0) {
+      fs.readdir('./data', (err, files) => {
+        if(err) {
+          console.error(err);
+        } else {
+          nextId = files.length + 1;
+          console.log('new ID' + nextId);
+        }
+        writeLog(nextId, data);
+        nextId++;
+        res.end();
+      });
+    } else {
+      writeLog(nextId, data);
+      nextId++;
+      res.end();
+    }
+  });
   res.end();
 });
 
